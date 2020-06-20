8676442502e7a98408b9f15e40358237e5b36e10
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-06-20 19:47:48 +0200

    C03 Insert invalid data bug

--- module-2/public/script.js
+++ module-2/public/script.js
@@ -3,29 +3,6 @@ function setup () {
   let lat
   let lon
 
-  const button = document.getElementById('submitButton')
-  button.addEventListener('click', async event => {
-    const mood = document.getElementById('mood')
-    if (!mood.value) {
-      mood.classList.add('error')
-      return
-    }
-    mood.classList.remove('error')
-    const moody = mood.value
-    mood.value = ''
-    const data = { lat, lon, moody }
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    }
-    const response = await fetch('/api', options)
-    const json = await response.json()
-    console.log(json)
-  })
-
   if ('geolocation' in navigator) {
     console.log('Yeah! Geolocation is available!')
 
@@ -35,21 +12,38 @@ function setup () {
 
       document.getElementById('latitude').textContent = latitude.toFixed(4)
       document.getElementById('longitude').textContent = longitude.toFixed(4)
-
-      const data = { latitude, longitude }
-      const options = {
-        method: 'POST',
-        header: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      }
-
-      const response = await fetch('/api', options)
-      const json = await response.json()
-      console.log(json)
     })
   } else {
     console.log('Looks like geolocation is not available')
   }
+
+  const button = document.getElementById('submitButton')
+  button.addEventListener('click', async event => {
+    const mood = document.getElementById('mood')
+
+    if (!mood.value) {
+      mood.classList.add('error')
+      return
+    }
+
+    mood.classList.remove('error')
+    const moody = mood.value
+    mood.value = ''
+
+    lat = Number(document.getElementById('latitude').innerHTML)
+    lon = Number(document.getElementById('longitude').innerHTML)
+
+    const data = { lat, lon, moody }
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+
+    const response = await fetch('/api', options)
+    const json = await response.json()
+    console.log(json)
+  })
 }
