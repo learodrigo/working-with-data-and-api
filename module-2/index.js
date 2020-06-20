43f8283658b4a3b3ac06351942f4b82f78210dfb
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-06-20 19:41:18 +0200

    C04 Saving to database

--- module-2/index.js
+++ module-2/index.js
@@ -1,16 +1,26 @@
 const express = require('express')
-const app = express()
+const Datastore = require('nedb')
 
+const app = express()
 app.listen(3000, () => { console.log('Go to http://localhost:3000...') })
 
 app.use(express.static('public'))
 app.use(express.json({ limit: '1mb' }))
 
+const database = new Datastore('database.db')
+database.loadDatabase()
+
 app.post('/api', (request, response) => {
-  console.log(request.body)
+  const data = request.body
+  const timestamp = Date.now()
+  data.timestamp = timestamp
+
+  database.insert(data)
+
   response.json({
     status: 'success',
-    latitude: request.body.lat,
-    longitude: request.body.lon
+    timestamp: timestamp,
+    latitude: data.lat,
+    longitude: data.lon
   })
 })
--- module-2/public/script.js
+++ module-2/public/script.js
@@ -1,26 +1,55 @@
-if ('geolocation' in navigator) {
-  console.log('Yeah! Geolocation is available!')
+function setup () {
+  noCanvas()
+  let lat
+  let lon
 
-  navigator.geolocation.getCurrentPosition(async position => {
-    const latitude = position.coords.latitude
-    const longitude = position.coords.longitude
-
-    document.getElementById('latitude').textContent = latitude
-    document.getElementById('longitude').textContent = longitude
-
-    const data = { latitude, longitude }
+  const button = document.getElementById('submitButton')
+  button.addEventListener('click', async event => {
+    const mood = document.getElementById('mood')
+    if (!mood.value) {
+      mood.classList.add('error')
+      return
+    }
+    mood.classList.remove('error')
+    const moody = mood.value
+    mood.value = ''
+    const data = { lat, lon, moody }
     const options = {
       method: 'POST',
-      header: {
+      headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
     }
-
     const response = await fetch('/api', options)
     const json = await response.json()
     console.log(json)
   })
-} else {
-  console.log('Looks like geolocation is not available')
+
+  if ('geolocation' in navigator) {
+    console.log('Yeah! Geolocation is available!')
+
+    navigator.geolocation.getCurrentPosition(async position => {
+      const latitude = position.coords.latitude
+      const longitude = position.coords.longitude
+
+      document.getElementById('latitude').textContent = latitude.toFixed(4)
+      document.getElementById('longitude').textContent = longitude.toFixed(4)
+
+      const data = { latitude, longitude }
+      const options = {
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }
+
+      const response = await fetch('/api', options)
+      const json = await response.json()
+      console.log(json)
+    })
+  } else {
+    console.log('Looks like geolocation is not available')
+  }
 }
