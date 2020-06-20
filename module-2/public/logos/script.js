396d286446041f028f1b55524237efe00faa6e16
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-06-20 22:05:35 +0200

    C07 Project wrap up

--- module-2/public/logos/script.js
+++ module-2/public/logos/script.js
@@ -14,12 +14,12 @@ async function getData () {
     const imag = document.createElement('img')
     imag.classList.add('image')
 
-
     mood.textContent = `mood: ${item.moody}`
     geo.textContent = `Geolocation - Lat ${item.lat.toFixed(2)}° Lon ${item.lon.toFixed(2)}°`
     const dateString = new Date(item.timestamp).toLocaleString()
     date.textContent = dateString
     imag.src = item.image64
+    img.alt = 'Random text'
 
     root.append(mood, geo, date, item.image64 ? imag : '')
     document.body.append(root)
